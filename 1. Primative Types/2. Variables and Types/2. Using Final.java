881207176970877773a5309bea4ba1e8a23452f0
Adam <adam@example.com>
2025-12-04 21:26:15 -0800

    add lession 1.2

---
+++ 1. Primative Types/2. Variables and Types/2. Using Final.java
@@ -0,0 +1,9 @@
+public class Final
+{
+    public static void main(String args[]) {
+        final int z;
+        z = 20;
+        z = 30;
+        System.out.println(z);
+    }
+}
---
+++ 1. Primative Types/2. Variables and Types/3. Our First Integer.java
@@ -0,0 +1,16 @@
+/*Write a program that declares an int named year and set
+it equal to the current year. Then print it out.
+
+If it were the year 2016, the output should say:
+
+The current year is 2016 */
+
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        int year = 2025;
+        System.out.println("The current year is " + year);
+    }
+}
