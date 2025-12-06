c7ddbc36220bd87eaeff688a5a6fa26a243f8052
Adam <adam@example.com>
2025-12-05 17:22:17 -0800

    add 2.1 and create some new folders

---
+++ 2. Using Objects/1. Objects - Instances of Classes.java/4. Pizza Instance Variables.java
@@ -0,0 +1,19 @@
+/*This class represents a Pizza.
+
+Add the instance variables you came up with in the last exercise to the class.
+
+Don’t forget to give the variables a type and a name. They should not have values at this point. */
+
+public class PizzaTester
+{
+    public static void main(String[] args)
+    {
+        System.out.println("You should be able to run this");
+        System.out.println("if you added the instance variables correctly");
+        String dough;
+        String sauce;
+        String toppings;
+        boolean stuffedCrust;
+        double price;
+    }
+}
---
+++ 2. Using Objects/1. Objects - Instances of Classes.java/5. Game Skeleton.java
@@ -0,0 +1,21 @@
+
+/**
+ * 
+ * The ConsoleGame class represents a console game.
+ * 
+ * Add the instance variables you think a console game would need
+ */ 
+
+public class ConsoleGame
+{
+
+    // Attributes
+    private String game;
+    private int xpos;
+    private int ypos;
+    private int zpos;
+    private int health;
+    private int level;
+    private int exp;
+    private String weapon;
+}
